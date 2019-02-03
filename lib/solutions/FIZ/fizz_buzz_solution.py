1b847b21b0ca3d4efa733c1f9509faeac1d52379
sbx_user1059 <sbx_user1059@ip-10-129-101-61.eu-west-2.compute.internal>
2019-02-03 20:19:53 +0000

    Sun Feb 03 20:19:53 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,3 +1,12 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    raise NotImplementedError()
+	div3=number%3==0
+	div5=number%5==0
+	if div3:
+		return "fizz"
+	elif div5:
+		return "buzz"
+	else:
+		return str(number)
+	raise NotImplementedError()
+
