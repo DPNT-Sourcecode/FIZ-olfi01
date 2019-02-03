713105f067c8267a711264b2477fbda286ea82b3
sbx_user1059 <sbx_user1059@ip-10-129-101-61.eu-west-2.compute.internal>
2019-02-03 20:56:46 +0000

    Sun Feb 03 20:56:46 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -4,6 +4,12 @@ def fizz_buzz(number):
 	div5=number%5==0
 	in3=str(3) in str(number)
 	in5=str(5) in str(number)
+	ll=[]
+	while number>0:
+		ll.append(number%10)
+		number=number//10
+	sm=len(set(ll)) == 1
+	dlx=(sm) and (number>10)
 	if (div3 or in3) and (div5 or in5):
 		return "fizz buzz"
 	elif div3 or in3:
@@ -13,3 +19,4 @@ def fizz_buzz(number):
 	else:
 		return str(number)
 	raise NotImplementedError()
+
