6b612ad2fde02d2ed137f0fc2b49a6ee283c8a9c
sbx_user1059 <sbx_user1059@ip-10-129-101-61.eu-west-2.compute.internal>
2019-02-03 21:38:52 +0000

    Sun Feb 03 21:38:52 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,44 +1,38 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-	a=number
-	isOdd=number%2!=0
-	div3=number%3==0
-	div5=number%5==0
-	in3=str(3) in str(number)
-	in5=str(5) in str(number)
-	ll=[]
-	while a>0:
-		ll.append(a%10)
-		a=a//10
-	sm=len(set(ll)) == 1
-	dlx3=(sm) and (number>10)
-	dlx= dlx3 and isOdd #this is fake delux
-	fx_dlx=dlx and ((div3 or in3) and (div5 or in5))
-	if (div3 or in3) and (div5 or in5):
-		if dlx:
-			return "fizz buzz fake deluxe"
-		elif dlx3:
-			return "fizz buzz deluxe"
-		else:
-			return "fizz buzz"
-	elif div3 or in3:
-		if dlx:
-			return "fizz fake deluxe"
-		elif dlx3:
-			return "fizz deluxe"
-		else:
-			return "fizz"
-	elif div5 or in5:
-		if dlx:
-			return "buzz fake deluxe"
-		elif dlx3:
-			return "buzz deluxe"
-		else:
-			return "buzz"
-	elif dlx3:
-		if isOdd:
-			return "fake deluxe"
-		else:
-			return "deluxe"
-	else:
-		return str(number)
+    isOdd=number%2!=0
+    div3=number%3==0
+    div5=number%5==0
+    in3=str(3) in str(number)
+    in5=str(5) in str(number)
+    dlx3=((div3 and in3) or (div5 and in5)) #this my delux
+    dlx= dlx3 and isOdd #this is fake delux 
+    if (div3 or in3) and (div5 or in5):
+        if dlx:
+            return "fizz buzz fake deluxe"
+        elif dlx3:
+            return "fizz buzz deluxe"
+        else:
+            return "fizz buzz"
+    elif div3 or in3:
+        if dlx:
+            return "fizz fake deluxe"
+        elif dlx3:
+            return "fizz deluxe"
+        else:
+            return "fizz"
+    elif div5 or in5:
+        if dlx:
+            return "buzz fake deluxe"
+        elif dlx3:
+            return "buzz deluxe"
+        else:
+            return "buzz"
+    elif dlx3:
+        if isOdd:
+            return "fake deluxe"
+        else:
+            return "deluxe"
+    else:
+        return str(number)
+    #raise NotImplementedError()
