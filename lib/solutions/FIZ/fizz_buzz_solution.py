d9614baef0ea4ecb82adbbfaa77741bcf42c5eac
sbx_user1059 <sbx_user1059@ip-10-129-101-61.eu-west-2.compute.internal>
2019-02-03 20:29:40 +0000

    Sun Feb 03 20:29:40 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,12 +2,15 @@
 def fizz_buzz(number):
 	div3=number%3==0
 	div5=number%5==0
-	if div3 and div5:
+	in3=str(3) in str(number)
+	in5=str(5) in str(number)
+	if (div3 or in3) and (div5 or in5):
 		return "fizz buzz"
-	elif div3:
+	elif div3 or in3:
 		return "fizz"
-	elif div5:
+	elif div5 or in5:
 		return "buzz"
 	else:
 		return str(number)
 	raise NotImplementedError()
+
