60af326095d70641ff50430dab5002bababb127d
sbx_user1059 <sbx_user1059@ip-10-129-101-61.eu-west-2.compute.internal>
2019-02-03 21:16:30 +0000

    Sun Feb 03 21:16:30 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,18 +1,21 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
+	a=number
 	div3=number%3==0
 	div5=number%5==0
 	in3=str(3) in str(number)
 	in5=str(5) in str(number)
 	ll=[]
-	while number>0:
-		ll.append(number%10)
-		number=number//10
+	while a>0:
+		ll.append(a%10)
+		a=a//10
 	sm=len(set(ll)) == 1
 	dlx=(sm) and (number>10)
 	fx_dlx=dlx and ((div3 or in3) and (div5 or in5))
 	if fx_dlx:
 		return "fizz buzz deluxe"
+	elif dlx:
+		return "deluxe"
 	elif (div3 or in3) and (div5 or in5):
 		return "fizz buzz"
 	elif div3 or in3:
@@ -22,4 +25,5 @@ def fizz_buzz(number):
 	else:
 		return str(number)
 	raise NotImplementedError()
+
 
